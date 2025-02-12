cbdcd96cc62e2b0da1cc96d40aeb2b5a5d8071d3
Peter Feng <37753741+Coypirus@users.noreply.github.com>
2025-02-11 23:38:34 -0500

    Set up timer skeleton

---
+++ src/timer.c
@@ -0,0 +1,17 @@
+#include "../include/timer.h"
+
+/**
+
+Write timer-specific interrupt handler here.
+As well as code to set up timers.
+
+Timer upper and lower registers combined are read_time.
+
+
+ */
+
+void read_time();
+
+void tick_in(int delay){}
+
+void set_timer_handler();

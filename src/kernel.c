2e5febea7b3fcd13d34a0607b90b0f83c89acd7e
Peter Feng <37753741+Coypirus@users.noreply.github.com>
2025-02-16 18:16:10 -0500

    Add timeout to uart

--- src/kernel.c
+++ src/kernel.c
@@ -13,7 +13,7 @@ void kernel_main(void)
     initialize_uart();
     
     struct shell kernel_shell;
-    kernel_shell.input = &recv_uart;
+    kernel_shell.input = &recv_with_timeout_uart;
     kernel_shell.output = &send_uart;
 
     run_shell(&kernel_shell);
